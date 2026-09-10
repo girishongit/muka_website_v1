ca565b3452859158c3ec2480e37d06dc78d95cec
Girish Bangalore Nagarajaiah <girish.bangalore.nagarajaiah@sap.com>
2026-09-10 23:27:08 +0200

    feat: wire up NuxtLayout/NuxtPage shell and default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="site-wrapper">
+    <TheHeader />
+    <main class="page-content">
+      <div class="container">
+        <slot />
+      </div>
+    </main>
+    <TheFooter />
+  </div>
+</template>
